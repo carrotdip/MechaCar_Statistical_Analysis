a5916a7205ee25c1e791b8a6567d45c6c523446e
carrotdip <92421274+carrotdip@users.noreply.github.com>
2022-02-28 16:55:15 -0800

    Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,10 @@
+# Deliverable 1
 # load dplyr package
 install.packages('dplyr')
 library('dplyr')
+# install ggplot2
+install.packages("ggplot2")
+library("ggplot2")
 # read csv file
 mecha_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 # perform linear regression
@@ -10,3 +14,11 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 # p-value = 5.35e-11 // r-squared = 0.7149
 # Pr(>|t|) is the probability that each coefficient contributes a random amount of variance to the linear model
 # Pr(>|t|): vehicle_length = 5.08e-8, vehicle_weight = 0.0776, spoiler_angle = 0.3069, ground_clearance = 5.21e-8, AWD = 0.1852
+
+# Deliverable 2
+# import csv file
+suspension_table <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
+# create total_summary dataframe
+total_summary <- suspension_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+# create lot_summary dataframe
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
